4aa718542a7354446bd5c3917706f223af95a678
Carrie Kraft <carriekarft@gmail.com>
2024-01-19 18:57:11 -0800

    added controller actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+    # skip_before_action :authorize, only: [:create]
+
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id 
+            render json: user
+        else
+            render json: {error: "Wrong username or password"}, status: 422
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+        render json: {}
+    end
 end
