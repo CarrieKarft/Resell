f20aa7a911891f7ef8df37f6fdd02a57b0c58b81
Carrie Kraft <carriekarft@gmail.com>
2024-01-19 19:13:28 -0800

    added API call methods

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useContext} from 'react';
+import ProductsPage from './components/ProductsPage';
+import NewProductForm from './components/NewProductForm';
+import {ProductsContext} from './context/ProductsContext'
+
+
 
 function App() {
+  const {setProducts} = useContext(ProductsContext);
+  
+  function onHandleCreateProduct(newProdObj) {
+    // console.log(data)
+    fetch('/products', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newProdObj)
+    })
+    .then((r) => {
+      if(r.ok) {
+          r.json().then((newProductData) => handleNewProductState(newProductData))
+      } else {
+          r.json().catch((errorData) => alert(errorData.errors))
+      }
+  })
+  }
+
+  function handleNewProductState(newProductData) {
+    console.log(newProductData.image_url)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProductsPage />
+      <NewProductForm onHandleCreateProduct={onHandleCreateProduct}/>
     </div>
   );
 }
