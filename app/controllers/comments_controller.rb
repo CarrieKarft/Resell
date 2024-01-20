86dcafc5dbc3392d87562a20132020f90245c8e4
Carrie Kraft <carriekarft@gmail.com>
2024-01-19 18:55:25 -0800

    added controller actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,28 @@
 class CommentsController < ApplicationController
+
+    def create
+        user = find_user
+        comment = user.comments.create!(comment_params)
+        render json: comment, status: :created
+    end
+
+    def update
+        user = find_user
+        comment = user.comments.find(params[:id])
+        comment.update!(comment_params)
+        render json: comment, status: 200
+    end
+
+    def destroy
+        user = find_user
+        comment = user.comments.find(params[:id])
+        comment.delete
+        render json: {}
+    end
+
+    private
+
+    def comment_params
+        params.permit(:product_id, :comment)
+    end
 end
