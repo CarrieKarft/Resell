3770c471d70e983f59d4934aeb036ae78b9ba5f1
Carrie Kraft <carriekarft@gmail.com>
2024-01-19 19:10:12 -0800

    created products context

---
+++ client/src/context/ProductsContext.js
@@ -0,0 +1,22 @@
+import {useState, useEffect, createContext} from 'react';
+
+const ProductsContext = createContext();
+
+function ProductsProvider({ children }) {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+      fetch('/products')
+      .then((r) => {
+          if(r.ok) {
+              r.json().then((productsData) => setProducts(productsData))
+          } else {
+              r.json().catch((errorData) => alert(errorData.errors))
+          }
+      })
+    }, [])
+
+    return <ProductsContext.Provider value={{products, setProducts}}>{children}</ProductsContext.Provider>;
+}
+
+export { ProductsContext, ProductsProvider };
