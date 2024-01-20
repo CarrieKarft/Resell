22ba9f28c843268576f316c364ebebb630e1557c
Carrie Kraft <carriekarft@gmail.com>
2024-01-19 18:57:57 -0800

    added controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+    # skip_before_action :authorize, only: [:create]
+
+
+    def show
+        render json: find_user, status: 200
+    end
+
+    def create
+        create_user = User.create!(user_params)
+        session[:user_id] = create_user.id
+        render json: create_user, status: :created
+    end
+
+    def index
+        users = User.all
+        render json: users, status: 200
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation, :email)
+    end
 end
