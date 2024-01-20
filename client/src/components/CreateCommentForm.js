a5bfee724fa83f4aa3e65e1b773501ac2afea373
Carrie Kraft <carriekarft@gmail.com>
2024-01-19 22:13:17 -0800

    created components

---
+++ client/src/components/CreateCommentForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CreateCommentForm() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default CreateCommentForm;
---
+++ client/src/components/EditCommentForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function EditCommentForm() {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+
+export default EditCommentForm;
