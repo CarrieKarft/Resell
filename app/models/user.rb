8be2a7ff0461e076bcfd6f0ce4c2984a35285527
Carrie Kraft <carriekarft@gmail.com>
2024-01-19 19:01:54 -0800

    added validations and association macros

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :password, confirmation: true
+    validates :password, :email, :username, presence: true
+    validates :email, uniqueness: true
+
     has_many :bids
     has_many :comments
     has_many :products, through: :bids
