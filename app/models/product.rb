65b24a908c7ceb4f2257f68030da2583ff00b8a1
Carrie Kraft <carriekarft@gmail.com>
2024-01-19 19:00:56 -0800

    added validations, associatin macros, andcustom method to access url of image object

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,13 @@
 class Product < ApplicationRecord
+    # validates :minimum_price, comparison: { greater_than: 4.99 }
+    validates :description, presence: true
+
     has_many :bids
     has_many :comments
     has_many :users, through: :bids
+    has_one_attached :image
+
+    def image_url
+        Rails.application.routes.url_helpers.url_for(image) if image.attached?
+    end
 end
