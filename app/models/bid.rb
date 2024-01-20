a6425044f1bb9a4631686ee5a606c319af54e2c5
Carrie Kraft <carriekarft@gmail.com>
2024-01-19 18:58:46 -0800

    added validations and association macros

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,4 +1,8 @@
 class Bid < ApplicationRecord
+    validates :bid_amount, presence: true
+    # will this work?
+    # validates :bid_amount, comparison: { greater_than: self.object.product.minimum_price }
+
     belongs_to :user
     belongs_to :product
 end
