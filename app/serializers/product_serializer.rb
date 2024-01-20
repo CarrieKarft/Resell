856bab0e9ea72c39ecb9a80989f107d1585a6eac
Carrie Kraft <carriekarft@gmail.com>
2024-01-19 19:04:33 -0800

    added image_url attribute

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,6 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :description, :minimum_price
+  attributes :id, :description, :minimum_price, :image, :image_url
+
+  has_many :comments
+  has_many :bids
 end
