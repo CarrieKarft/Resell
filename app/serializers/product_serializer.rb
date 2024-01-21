332754337bf72b7542654bbcd69926548c8f2f3d
Carrie Kraft <carriekarft@gmail.com>
2024-01-20 21:27:08 -0800

    added highest_bid attribute

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,6 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :description, :minimum_price, :image, :image_url
+  attributes :id, :description, :minimum_price, :image, :image_url, :highest_bid, :product_name
+  # might change highest bid to be a controller action so can repetedly fetch since it will change frequently, but this is fine for now
 
   has_many :comments
   has_many :bids
