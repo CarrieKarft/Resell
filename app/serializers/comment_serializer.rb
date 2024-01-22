5b6b50e4b160e7b1b88642df9d865be611fc39b3
Carrie Kraft <carriekarft@gmail.com>
2024-01-21 23:02:06 -0800

    added custom method to return the username of the user who left the comment

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,3 +1,8 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :comment, :user_id, :product_id
+  attributes :id, :comment, :user_id, :product_id, :comment_username
+  
+  
+  def comment_username
+    self.object.user.username
+  end
 end
