7a5dcfddf9dfc6f4913d295e1823719cb9d8315a
Carrie Kraft <carriekarft@gmail.com>
2024-01-21 20:48:59 -0800

    added username display

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useContext} from "react";
+import { CurrentUserContext } from '../context/CurrentUserContext'
+import ProductCard from "./ProductCard";
 
 function UserProfile({onHandleLogout}) {
+    const {currentUser} = useContext(CurrentUserContext)
+    const {products, username} = currentUser
+    console.log(currentUser)
+    console.log(products)
+
+    // const mappingUserBids = products.map((prod) => {
+    //     return <ProductCard prod={prod} key={prod.id}/>
+    // })
     return (
         <div>
             <h1>User Profile</h1>
             <button onClick={() => onHandleLogout()}>Logout</button>
-
+            <h2>Hello, {username}</h2>
+            {/* <div>{mappingUserBids}</div> */}
         </div>
     )
 }
