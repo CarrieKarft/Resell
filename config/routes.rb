eb340c094b8f89e37bda93cc8870c636fb22a57d
Carrie Kraft <carriekarft@gmail.com>
2024-01-21 23:22:19 -0800

    commenting out namespacing for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  
-  resources :bids
-  resources :comments
-  resources :products
-  resources :users, except: [:show, :create]
-  get '/search-products/:product_name', to: "products#search_products"
-  post '/signup', to: "users#create"
-  get '/me', to: "users#show"
-  delete '/logout', to: "sessions#destroy"
-  post '/login', to: "sessions#create"
+  # namespace :api do
+    resources :bids
+    resources :comments
+    resources :products
+    resources :users, except: [:show, :create]
+    get '/search-products/:product_name', to: "products#search_products"
+    post '/signup', to: "users#create"
+    get '/me', to: "users#show"
+    delete '/logout', to: "sessions#destroy"
+    post '/login', to: "sessions#create"
+  # end
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
