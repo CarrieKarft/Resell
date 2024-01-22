281d169f12031d75653a3aa93bb2e93136a7bff5
Carrie Kraft <carriekarft@gmail.com>
2024-01-21 23:04:59 -0800

    added username of user who left comment

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -2,9 +2,10 @@ import React from "react";
 
 function Comment({comm}) {
     console.log(comm)
-    const {comment, product_id} = comm
+    const {comment, product_id, comment_username} = comm
     return (
         <div>
+            <h4>{comment_username}:</h4>
             <p>{comment}</p>
         </div>
     )
