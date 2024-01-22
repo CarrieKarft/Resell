346dfdf00b9e111c304afbab862cb6721e0770a3
Carrie Kraft <carriekarft@gmail.com>
2024-01-21 20:46:13 -0800

    added product image and product name

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -5,13 +5,25 @@ import CreateCommentForm from "./CreateCommentForm";
 
 
 function ProductCard({prod}) {
-    console.log(prod.bids)
-    const {description, minimum_price, image} = prod
+    console.log(prod)
+    const {description, minimum_price, image_url, bids, highest_bid, comments, product_name} = prod
+
+    const mappingComments = comments.map(comm => <Comment key={comm.id} comm={comm}/>)
+
+    
     return (
         <div>
-            <img></img>
-            <h2>{description}</h2>
+            <img src={image_url} alt="product picture" className="productImage"/>
+            <h2>{product_name}</h2>
+            <p>{description}</p>
             <p>{minimum_price}</p>
+            <div>
+                {(!!highest_bid ? <h3>Current Highest Bid: {highest_bid}</h3> : <h3>place your bid now</h3>)}
+            </div>
+            <div>
+                <h3>comments</h3>
+                {mappingComments}
+            </div>
         </div>
     )
 }
