26d759b03422be21197cb3040ae239a87a964dc1
Carrie Kraft <carriekarft@gmail.com>
2024-01-21 20:44:31 -0800

    added buttons to visualize component

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,7 +3,9 @@ import React from "react";
 function NavBar() {
     return(
         <div>
-
+            <button>UserProfile</button> 
+            <button>View Your bids</button>
+            <button>View Products</button>
         </div>
     )
 }
