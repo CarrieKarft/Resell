ae08a2d16e1f201585eb798f401fadcdf92982ac
Carrie Kraft <carriekarft@gmail.com>
2024-01-21 20:50:59 -0800

    updated schema with product name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_17_205804) do
+ActiveRecord::Schema.define(version: 2024_01_20_072414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2024_01_17_205804) do
     t.float "minimum_price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "product_name"
   end
 
   create_table "users", force: :cascade do |t|
