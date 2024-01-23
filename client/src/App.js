289e3b145592227aedb62f5578880b3cbfd8ff7d
Carrie Kraft <carriekarft@gmail.com>
2024-01-22 20:37:31 -0800

    added API calls for creating, updating, and deleting comments, as well as updating products state to reflect the changes to comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react';
-import {Routes, Route, useNavigate} from "react-router-dom";
+import {Routes, Route, useNavigate, json} from "react-router-dom";
 import ProductsPage from './components/ProductsPage';
 import NewProductForm from './components/NewProductForm';
 import {ProductsContext} from './context/ProductsContext'
@@ -15,7 +15,7 @@ import UserProfile from './components/UserProfile';
 function App() {
   const {products, setProducts} = useContext(ProductsContext);
   const {currentUser, setCurrentUser} = useContext(CurrentUserContext)
-
+  console.log(products)
   if(!currentUser) return <SignupPage  onHandleLoginFetch={onHandleLoginFetch} handleUserSignupFetch={handleUserSignupFetch}/>
 
   function onHandleLoginFetch(loginObj) {
@@ -79,15 +79,94 @@ function App() {
     const addingNewProduct = [...products, newProductData]
     setProducts(addingNewProduct)
   }
+
+  function onHandelCreatingNewComment(newCommentObj) {
+    console.log(newCommentObj)
+    fetch('/comments', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newCommentObj)
+    }).then(r => {
+      if(r.ok) {
+          r.json().then(newCommentData => handleUpdatingCommentState(newCommentData))
+      } else {
+          r.json().then(errorData => alert(errorData.error))
+      }
+    })
+  }
+
+  function handleUpdatingCommentState(newCommentData) {
+    console.log(newCommentData)
+    const findingProduct = products.find(prod => prod.id === newCommentData.product_id)
+    const updatingProdComments = [...findingProduct.comments, newCommentData]
+    const updatingProd = {...findingProduct, comments: updatingProdComments}
+    const replacingProd = products.map(prod => prod.id === findingProduct.id ? updatingProd : prod)
+    setProducts(replacingProd)
+
+  }
+
+  function onHandleUpdatingComment(comment, id) {
+    console.log(comment)
+    console.log(id)
+    fetch(`/comments/${id}`,  {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({comment: comment})
+    }).then(r => {
+      if(r.ok) {
+          r.json().then(updatedCommentData => handleStateForUpdatedComment(updatedCommentData))
+      } else {
+          r.json().then(errorData => alert(errorData.error))
+      }
+    })
+  }
+
+  function handleStateForUpdatedComment(updatedCommentData) {
+    const findingProduct = products.find(prod => prod.id === updatedCommentData.product_id);
+    const replacingComment = findingProduct.comments.map(comm => comm.id === updatedCommentData.id ? updatedCommentData : comm);
+    const updatedProduct = {...findingProduct, comments: replacingComment};
+    const replacingProduct = products.map(prod => prod.id === updatedProduct.id ? updatedProduct : prod);
+    setProducts(replacingProduct)
+  }
+
+  function onHandleDelete(id, product_id) {
+    console.log(id)
+    fetch(`/comments/${id}`, {
+      method: "DELETE",
+    })
+    .then(r => {
+      if(r.ok) {
+        r.json().then(() => handleRemovingComment(id, product_id))
+      } else {
+        r.json().then(errorData => alert(errorData.error))
+      }
+    })
+  }
+
+  function handleRemovingComment(id, product_id) {
+    console.log(id)
+    console.log(product_id)
+    const findingProduct = products.find(prod => prod.id === product_id);
+    const filterOutComment = findingProduct.comments.filter(comm => comm.id !== id);
+    const updatingProduct = {...findingProduct, comments: filterOutComment};
+    const replacingProduct = products.map(prod => prod.id === updatingProduct.id ? updatingProduct : prod);
+    setProducts(replacingProduct);
+  }
+
+
   return (
     <div className="App">
       
       <NavBar username={currentUser.username}/>
       <Routes>
-        {/* <SignupPage handleUserSignupFetch={handleUserSignupFetch}/> */}
         <Route path='/profile' element={<UserProfile onHandleLogout={onHandleLogout}/>} />
         <Route path='/products-page' element={<ProductsPage />} />
         <Route path='/product/new' element={<NewProductForm onHandleCreateProduct={onHandleCreateProduct}/>} />
+        <Route path='/product/:id' element={<ViewProduct onHandelCreatingNewComment={onHandelCreatingNewComment} onHandleUpdatingComment={onHandleUpdatingComment} onHandleDelete={onHandleDelete}/>}/>
       </Routes>
     </div>
   );
