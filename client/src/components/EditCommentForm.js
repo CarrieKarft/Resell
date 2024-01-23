3332b3dd5a3ee7b3decf657cd1dc7007d946916d
Carrie Kraft <carriekarft@gmail.com>
2024-01-22 20:53:07 -0800

    built controlled form that involes a callback function to update a comment

--- client/src/components/EditCommentForm.js
+++ client/src/components/EditCommentForm.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 
-function EditCommentForm() {
+function EditCommentForm({comm, onHandleUpdatingComment, onHandleDelete, setClicked}) {
+    const {id, product_id} = comm
+    const [comment, setComment] = useState(comm.comment)
+    console.log(comm)
+
+    function handleSubmit(e) {
+        e.preventDefault() 
+        onHandleUpdatingComment(comment, id)
+        setClicked(false)
+    }
     return(
         <div>
-
+            <div>
+                <button onClick={() => setClicked(false)}>Minimize</button>
+                <button onClick={() => onHandleDelete(id, product_id)}>Delete Comment</button>
+            </div>
+            <div>
+                <form onSubmit={e => handleSubmit(e)}>
+                    <label>
+                        <textarea
+                        type='text'
+                        value={comment}
+                        onChange={e => setComment(e.target.value)}
+                        >
+                        </textarea>
+                    </label>
+                    <br />
+                    <input type='submit'></input>
+                </form>
+            </div>
         </div>
     )
 }
