e1816dba04f68cf1d2f2540804765ea90b62ff99
Carrie Kraft <carriekarft@gmail.com>
2024-01-22 20:55:00 -0800

    removed some elements since they are displayed when you click on the product, and added class names for styling

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import Bid from "./Bid";
 import Comment from "./Comment";
 import CreateCommentForm from "./CreateCommentForm";
@@ -6,24 +7,14 @@ import CreateCommentForm from "./CreateCommentForm";
 
 function ProductCard({prod}) {
     console.log(prod)
-    const {description, minimum_price, image_url, bids, highest_bid, comments, product_name} = prod
+    const {image_url, product_name, id} = prod
 
-    const mappingComments = comments.map(comm => <Comment key={comm.id} comm={comm}/>)
-
-    
     return (
-        <div>
-            <img src={image_url} alt="product picture" className="productImage"/>
+        <div className="productCard">
             <h2>{product_name}</h2>
-            <p>{description}</p>
-            <p>{minimum_price}</p>
-            <div>
-                {(!!highest_bid ? <h3>Current Highest Bid: {highest_bid}</h3> : <h3>place your bid now</h3>)}
-            </div>
-            <div>
-                <h3>comments</h3>
-                {mappingComments}
-            </div>
+            <img src={image_url} alt="product picture" className="productImage"/>
+            <br />
+            <NavLink to={`/product/${id}`}>View Details</NavLink>
         </div>
     )
 }
