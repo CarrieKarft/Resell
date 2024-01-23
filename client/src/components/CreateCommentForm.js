ad7d70baeadf00cfd627261ee8eadd7bef830a03
Carrie Kraft <carriekarft@gmail.com>
2024-01-22 20:51:58 -0800

    built controlled form

--- client/src/components/CreateCommentForm.js
+++ client/src/components/CreateCommentForm.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
-function CreateCommentForm() {
+function CreateCommentForm({product_id, onHandelCreatingNewComment}) {
+    const [comment, setComment] = useState('')
+    function handleSubmit(e) {
+        e.preventDefault()
+        const newCommentObj = {
+            comment,
+            product_id
+        }
+        onHandelCreatingNewComment(newCommentObj)
+        setComment('')
+    }
     return (
         <div>
+            <h3><em>Leave A Comment</em></h3>
+            <form onSubmit={e => handleSubmit(e)}>
+                <label>
+                    <textarea
+                    type='text'
+                    value={comment}
+                    onChange={e => setComment(e.target.value)}
+                    >
+                    </textarea>
+                </label>
+                <br />
+                <input type='submit'></input>
+            </form>
 
         </div>
     )
