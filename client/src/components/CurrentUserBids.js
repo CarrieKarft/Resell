c863e4e03b6718ebe6957e208e8d2ee7a9132a46
Carrie Kraft <carriekarft@gmail.com>
2024-01-24 02:02:26 -0800

    added a component to render just products that a user previously bid on

--- client/src/components/CurrentUserBids.js
+++ client/src/components/CurrentUserBids.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {useContext} from "react";
+import {CurrentUserContext} from '../context/CurrentUserContext'
+import ProductCard from "./ProductCard";
 
 function CurrentUserBids() {
+    const {currentUser} = useContext(CurrentUserContext);
+
+    const {products} = currentUser
+
+    if (!products) return <h2>Loading...</h2>
+
+    const mappingUserProducts = currentUser.products.map(prod => <ProductCard key={prod.id} prod={prod}/>)
+
     return(
         <div>
-
+            <h2>Products You've Bid On</h2>
+            <div className="productCardContainer">
+                {mappingUserProducts}
+            </div>
         </div>
     )
 }
