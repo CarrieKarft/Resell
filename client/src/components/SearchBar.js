0f7cf22f2843580fb55ac99ecfa2ce6b33fe28fd
Carrie Kraft <carriekarft@gmail.com>
2024-01-24 02:07:21 -0800

    created form to capture user input for search field

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
-function SearchBar() {
+function SearchBar({onHandleSearchCall}) {
+    const [searchWord, setSearchWord] = useState('')
+
+    function handleSearchSubmit(e) {
+        e.preventDefault()
+        onHandleSearchCall(searchWord)
+    }
     return (
         <div>
+            <form onSubmit={e => handleSearchSubmit(e)}>
+                <label>Search For Products
+                    <input
+                    type='text'
+                    vlaue={searchWord}
+                    onChange={e => setSearchWord(e.target.value)}
+                    >
+                    </input>
+                </label>
+                <input type='submit'></input>
+            </form>
 
         </div>
     )
