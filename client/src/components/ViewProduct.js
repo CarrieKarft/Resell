c65df7924e09280e4c100e4dbe1306d6f10aed07
Carrie Kraft <carriekarft@gmail.com>
2024-01-24 02:09:41 -0800

    added function to check if user has previously bid on the product

--- client/src/components/ViewProduct.js
+++ client/src/components/ViewProduct.js
@@ -1,16 +1,23 @@
 import React, {useContext} from "react";
 import {useParams} from  'react-router-dom';
 import { ProductsContext } from '../context/ProductsContext'
+import { CurrentUserContext } from "../context/CurrentUserContext";
 import Comment from "./Comment";
 import CreateCommentForm from "./CreateCommentForm";
+import Bid from "./Bid";
 
-function ViewProduct({onHandelCreatingNewComment, onHandleUpdatingComment, onHandleDelete}) {
+function ViewProduct({onHandelCreatingNewComment, onHandleUpdatingComment, onHandleDelete, onHandleUpdatingBid, onHandleCreateBid}) {
     const {id} = useParams()
     const {products} = useContext(ProductsContext)
+    const {currentUser} = useContext(CurrentUserContext)
+    // console.log(currentUser.bids)
 
     const findProduct = products.find(prod => prod.id == id)
     if (!findProduct) return <h2>Loading...</h2>
     const {image_url, comments, highest_bid, product_name, description, minimum_price} = findProduct
+
+    const checkingForPreviousBid = currentUser.bids.find(bid => bid.product_id === findProduct.id)
+    // console.log(checkingForPreviousBid)
 
     const mappingComments = comments.map(comm => <Comment key={comm.id} comm={comm} onHandleUpdatingComment={onHandleUpdatingComment} onHandleDelete={onHandleDelete}/>)
     return(
@@ -21,8 +28,12 @@ function ViewProduct({onHandelCreatingNewComment, onHandleUpdatingComment, onHan
                 <p>{description}</p>
                 <p>{minimum_price}</p>
                 <div>
-                    {(!!highest_bid ? <h3>Current Highest Bid: {highest_bid}</h3> : <h3>place your bid now</h3>)}
+                <Bid checkingForPreviousBid={checkingForPreviousBid} highest_bid={highest_bid} onHandleUpdatingBid={onHandleUpdatingBid} product_id={id} onHandleCreateBid={onHandleCreateBid}/>
                 </div>
+                {/* <div className="productBidDiv">
+                    {(!!highest_bid ? <h3>Current Highest Bid: {highest_bid}</h3> : null)}
+                    {(checkingForPreviousBid ? <button onClick={() => handleBidUpdate()}>Update Your Bid</button> : <button>Place A Bid</button>)}
+                </div> */}
             </div>
             <div className="bottomHalf">
                 <div>
