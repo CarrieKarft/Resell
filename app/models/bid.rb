3929c13e1b3fe8a8db16abe42385db8161d13e26
Carrie Kraft <carriekarft@gmail.com>
2024-01-24 01:54:31 -0800

    deleted comments

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,8 +1,6 @@
 class Bid < ApplicationRecord
     validates :bid_amount, presence: true
-    # will this work?
-    # validates :bid_amount, comparison: { greater_than: self.object.product.minimum_price }
-
+    
     belongs_to :user
     belongs_to :product
 end
