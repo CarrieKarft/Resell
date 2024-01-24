4f6df6a4c90fe39d11408dc054e6f2a971156d08
Carrie Kraft <carriekarft@gmail.com>
2024-01-24 01:58:57 -0800

    built out bid component to render and option to update a bid or create a bid based on weather a user has already placed a bid on the item

--- client/src/components/Bid.js
+++ client/src/components/Bid.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
+import UpdateBidForm from "./UpdateBidForm";
+import CreateBidForm from "./CreateBidForm";
 
-function Bid({bid}) {
-    console.log(bid)
+function Bid({highest_bid, checkingForPreviousBid, onHandleUpdatingBid, product_id, onHandleCreateBid}) {
+    const [updateClicked, setUpdateClicked] = useState(false)
+    const [createClicked, setCreateClicked] = useState(false)
+    // console.log(checkingForPreviousBid)
+
     return(
         <div>
-            <h2>Bid</h2>
+            <div className="productBidDiv">
+                {(!!highest_bid ? <h3>Current Highest Bid: ${highest_bid}</h3> : null)}
+                {(checkingForPreviousBid ? <div className="previousBid"><h4>Your current bid: ${checkingForPreviousBid.bid_amount}</h4><button onClick={() => setUpdateClicked(!updateClicked)}>Update Your Bid</button></div> : <div className='newBid'><button onClick={() => setCreateClicked(!createClicked)}>Place A Bid</button></div>)}
+            </div>
+            <div style={{display: createClicked ? null : "none"}}>
+                <CreateBidForm setCreateClicked={setCreateClicked} product_id={product_id} onHandleCreateBid={onHandleCreateBid}/>
+            </div>
+            <div style={{display: updateClicked ? null : "none"}}>
+                <UpdateBidForm checkingForPreviousBid={checkingForPreviousBid} setUpdateClicked={setUpdateClicked} onHandleUpdatingBid={onHandleUpdatingBid}/>
+            </div>
         </div>
     )
 }
