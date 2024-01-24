8fd4f49db3c95996b7642341aef74986cbca6afa
Carrie Kraft <carriekarft@gmail.com>
2024-01-24 02:03:44 -0800

    updated navbar to include the name of the application

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,11 +3,14 @@ import {NavLink} from "react-router-dom"
 
 function NavBar({username}) {
     return(
+        <div className="nav">
+            <h1>RESELL</h1>
         <div className="navBar">
             <NavLink to='/profile'>{username}'s profile</NavLink> 
-            <NavLink to='/profile'>View Your bids</NavLink>
+            <NavLink to='/bids'>View Your bids</NavLink>
             <NavLink to='/products-page'>View Products</NavLink>
             <NavLink to='/product/new'>Upload A New Product</NavLink>
+        </div>
         </div>
     )
 }
