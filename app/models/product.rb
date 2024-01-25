ff001782fa647e56144ab361065f7353220ff41b
Carrie Kraft <carriekarft@gmail.com>
2024-01-24 20:01:31 -0800

    changed highest bid method to get the entire bid object instead of just the amount

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,9 @@
 class Product < ApplicationRecord
     # validates :minimum_price, comparison: { greater_than: 4.99 }
-    validates :description, :product_name, presence: true
+    validates :description, presence: true
 
     has_many :bids
+    # need to add dependent destroy for comments
     has_many :comments
     has_many :users, through: :bids
     has_one_attached :image
@@ -11,11 +12,11 @@ class Product < ApplicationRecord
         Rails.application.routes.url_helpers.url_for(image) if image.attached?
     end
 
-    def highest_bid
+    def current_highest_bid
         if self.bids.length > 0
-            self.bids.maximum(:bid_amount)
+            self.bids.find_by(bid_amount: self.bids.maximum(:bid_amount))
         else
-            0.00
+            {bid_amount: 0.00}
         end
     end
 end
