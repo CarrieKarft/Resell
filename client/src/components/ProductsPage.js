8cefa973d51f05e698537eb8b622b0315a4d9e63
Carrie Kraft <carriekarft@gmail.com>
2024-01-25 14:00:31 -0800

    removed unneeded comments

--- client/src/components/ProductsPage.js
+++ client/src/components/ProductsPage.js
@@ -1,18 +1,15 @@
 import React, {useContext} from "react";
 import {ProductsContext} from '../context/ProductsContext'
 import ProductCard from "./ProductCard";
-import SearchBar from "./SearchBar";
 
 
-function ProductsPage({onHandleSearchCall}){
+function ProductsPage({}){
     const {products} = useContext(ProductsContext);
-    // console.log(products)
 
     const mappingProducts = products.map(prod => <ProductCard key={prod.id} prod={prod}/>)
     return (
         <div className="poductsPage">
             <h1>Products Page</h1>
-            <SearchBar onHandleSearchCall={onHandleSearchCall}/>
             <div className="productCardContainer">
                 {mappingProducts}
             </div>
