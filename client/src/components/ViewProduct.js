ea8219e54b71aa6599c7ff1f22c8737d8e4291e3
Carrie Kraft <carriekarft@gmail.com>
2024-01-24 20:07:18 -0800

    updated to reflect current highest bid object and render bid timer

--- client/src/components/ViewProduct.js
+++ client/src/components/ViewProduct.js
@@ -1,12 +1,13 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import {useParams} from  'react-router-dom';
 import { ProductsContext } from '../context/ProductsContext'
 import { CurrentUserContext } from "../context/CurrentUserContext";
 import Comment from "./Comment";
 import CreateCommentForm from "./CreateCommentForm";
 import Bid from "./Bid";
+import BidTimer from "./BidTimer";
 
-function ViewProduct({onHandelCreatingNewComment, onHandleUpdatingComment, onHandleDelete, onHandleUpdatingBid, onHandleCreateBid}) {
+function ViewProduct({onHandelCreatingNewComment, onHandleUpdatingComment, onHandleDelete, onHandleUpdatingBid, onHandleCreateBid, onHandleUpdatingWinningBid, onHandleUpdatingNonWinner}) {
     const {id} = useParams()
     const {products} = useContext(ProductsContext)
     const {currentUser} = useContext(CurrentUserContext)
@@ -14,26 +15,27 @@ function ViewProduct({onHandelCreatingNewComment, onHandleUpdatingComment, onHan
 
     const findProduct = products.find(prod => prod.id == id)
     if (!findProduct) return <h2>Loading...</h2>
-    const {image_url, comments, highest_bid, product_name, description, minimum_price} = findProduct
-
+    if (!currentUser.bids) return <h2>Loading...</h2>
+    const {image_url, comments, current_highest_bid, product_name, description} = findProduct
+    console.log(current_highest_bid)
+// this line is taking a while to load? how can I make it faster?
     const checkingForPreviousBid = currentUser.bids.find(bid => bid.product_id === findProduct.id)
-    // console.log(checkingForPreviousBid)
 
     const mappingComments = comments.map(comm => <Comment key={comm.id} comm={comm} onHandleUpdatingComment={onHandleUpdatingComment} onHandleDelete={onHandleDelete}/>)
+
+
     return(
         <div className="viewProduct">
             <div className="topHalf">
                 <h2>{product_name}</h2>
                 <img src={image_url} alt="product picture" className="productImage"/>
                 <p>{description}</p>
-                <p>{minimum_price}</p>
                 <div>
-                <Bid checkingForPreviousBid={checkingForPreviousBid} highest_bid={highest_bid} onHandleUpdatingBid={onHandleUpdatingBid} product_id={id} onHandleCreateBid={onHandleCreateBid}/>
+                    <BidTimer current_highest_bid={current_highest_bid} findProduct={findProduct} onHandleUpdatingWinningBid={onHandleUpdatingWinningBid} onHandleUpdatingNonWinner={onHandleUpdatingNonWinner}/>
                 </div>
-                {/* <div className="productBidDiv">
-                    {(!!highest_bid ? <h3>Current Highest Bid: {highest_bid}</h3> : null)}
-                    {(checkingForPreviousBid ? <button onClick={() => handleBidUpdate()}>Update Your Bid</button> : <button>Place A Bid</button>)}
-                </div> */}
+                <div >
+                {current_highest_bid.bid_accepted === true ? <h3>You can no longer bid on this item</h3> : <Bid checkingForPreviousBid={checkingForPreviousBid} current_highest_bid={current_highest_bid} onHandleUpdatingBid={onHandleUpdatingBid} product_id={id} onHandleCreateBid={onHandleCreateBid}/>}
+                </div>
             </div>
             <div className="bottomHalf">
                 <div>
