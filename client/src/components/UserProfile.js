c4f99b86fd9c6b1dac3eeac140241b09f4d0bdfc
Carrie Kraft <carriekarft@gmail.com>
2024-01-24 20:06:25 -0800

    displayed bids that the current user has won

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -5,8 +5,25 @@ import ProductCard from "./ProductCard";
 function UserProfile({onHandleLogout}) {
     const {currentUser} = useContext(CurrentUserContext)
     const {products, username} = currentUser
-    // console.log(currentUser)
-    // console.log(products)
+    if (!currentUser.bids) return <h2>Loading...</h2>
+    const winningBids = currentUser.bids.filter(bid => bid.bid_accepted === true).map(bid => bid.product_id)
+    console.log(winningBids)
+    const gettingProducts = products.filter(prod => winningBids.includes(prod.id)).map(prod => {
+        return (
+            <div key={prod.id}>
+                <h3>Congrats! You've won some bids</h3>
+                <div className="productCardContainer">
+                    <ProductCard prod={prod} key={prod.id}/>
+                </div>
+            </div>
+        )
+    
+})
+    // const gettingProductId = winningBids.map(bid)
+    // console.log(gettingProducts)
+    // console.log(gettingProductId)
+
+    
 
 
     return (
@@ -14,6 +31,7 @@ function UserProfile({onHandleLogout}) {
             <h1>User Profile</h1>
             <button onClick={() => onHandleLogout()}>Logout</button>
             <h2>Hello, {username}</h2>
+            {gettingProducts}
         </div>
     )
 }
