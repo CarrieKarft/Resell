9408cbf6b42e1833a26129db690ed16e7dd10b3d
Carrie Kraft <carriekarft@gmail.com>
2024-01-24 20:05:35 -0800

    updated to reflect current highest bid object

--- client/src/components/Bid.js
+++ client/src/components/Bid.js
@@ -2,15 +2,18 @@ import React, {useState} from "react";
 import UpdateBidForm from "./UpdateBidForm";
 import CreateBidForm from "./CreateBidForm";
 
-function Bid({highest_bid, checkingForPreviousBid, onHandleUpdatingBid, product_id, onHandleCreateBid}) {
+function Bid({current_highest_bid, checkingForPreviousBid, onHandleUpdatingBid, product_id, onHandleCreateBid}) {
     const [updateClicked, setUpdateClicked] = useState(false)
     const [createClicked, setCreateClicked] = useState(false)
     // console.log(checkingForPreviousBid)
 
+    console.log(current_highest_bid)
+    const {bid_amount} = current_highest_bid
+
     return(
         <div>
             <div className="productBidDiv">
-                {(!!highest_bid ? <h3>Current Highest Bid: ${highest_bid}</h3> : null)}
+                {(bid_amount > 0.00 ? <h3>Current Highest Bid: ${bid_amount}</h3> : null)}
                 {(checkingForPreviousBid ? <div className="previousBid"><h4>Your current bid: ${checkingForPreviousBid.bid_amount}</h4><button onClick={() => setUpdateClicked(!updateClicked)}>Update Your Bid</button></div> : <div className='newBid'><button onClick={() => setCreateClicked(!createClicked)}>Place A Bid</button></div>)}
             </div>
             <div style={{display: createClicked ? null : "none"}}>
