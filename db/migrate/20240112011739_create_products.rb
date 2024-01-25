dc2e79db0ac0e16d5bdc9ffbfa57448d1868661d
Carrie Kraft <carriekarft@gmail.com>
2024-01-24 20:17:42 -0800

    removed un-used attribute

--- db/migrate/20240112011739_create_products.rb
+++ db/migrate/20240112011739_create_products.rb
@@ -2,7 +2,6 @@ class CreateProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :products do |t|
       t.string :description
-      t.float :minimum_price
 
       t.timestamps
     end
