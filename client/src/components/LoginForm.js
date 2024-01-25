fe35673652bb3c6654f27b2bae877c5a34c22375
Carrie Kraft <carriekarft@gmail.com>
2024-01-25 14:34:24 -0800

    removed comments and unused attributes

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from "react";
+import React, {useState} from "react";
 
 function LoginForm({onHandleLoginFetch}) {
     const [username, setUsername] = useState('')
