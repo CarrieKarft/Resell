8c431768b17eef14aa99ab7270a04ca79089bd90
Carrie Kraft <carriekarft@gmail.com>
2024-01-26 14:23:06 -0800

    added conditional rendering to display either the current highest bid or that bds are no longer accepted on the product in the listing page

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -3,12 +3,13 @@ import { NavLink } from "react-router-dom";
 
 
 function ProductCard({prod}) {
-    const {image_url, product_name, id} = prod
+    const {image_url, product_name, id, current_highest_bid} = prod
 
     return (
         <div className="productCard">
             <h2>{product_name}</h2>
             <img src={image_url} alt={product_name} className="productImage"/>
+            <div>{current_highest_bid.bid_accepted === false ? <h4>Current Highest Bid: ${current_highest_bid.bid_amount}</h4> : <h4>You Can No Longer Bid On This Item</h4>}</div>
             <br />
             <NavLink to={`/product/${id}`}>View Details</NavLink>
         </div>
