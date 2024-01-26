4d524e6dc4d1ebd751030ebb94d7e61bd91e9598
Carrie Kraft <carriekarft@gmail.com>
2024-01-25 21:39:47 -0800

    added dependedt destroy to delete associated comments when a product is deleted

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,8 @@
 class Product < ApplicationRecord
-    # validates :minimum_price, comparison: { greater_than: 4.99 }
     validates :description, presence: true
 
     has_many :bids
-    # need to add dependent destroy for comments
-    has_many :comments
+    has_many :comments, dependent: :destroy
     has_many :users, through: :bids
     has_one_attached :image
 
