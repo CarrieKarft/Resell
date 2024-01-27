2e9e80292836c95e9978d6663a82e9e6aa9b0c5e
Carrie Kraft <carriekarft@gmail.com>
2024-01-26 23:11:49 -0800

    added user_id

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,5 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :description, :image, :image_url, :product_name, :current_highest_bid
+  attributes :id, :description, :image, :image_url, :product_name, :current_highest_bid, :user_id
 
 
   has_many :comments
