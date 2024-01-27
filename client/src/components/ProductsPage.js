1ee31d7756ffaf9387e3d9e245a899af2901cdff
Carrie Kraft <carriekarft@gmail.com>
2024-01-26 23:19:47 -0800

    destructured out id from current user to pass to each individual product card

--- client/src/components/ProductsPage.js
+++ client/src/components/ProductsPage.js
@@ -1,12 +1,15 @@
 import React, {useContext} from "react";
 import {ProductsContext} from '../context/ProductsContext'
+import { CurrentUserContext } from "../context/CurrentUserContext";
 import ProductCard from "./ProductCard";
 
 
 function ProductsPage(){
     const {products} = useContext(ProductsContext);
+    const {currentUser} = useContext(CurrentUserContext)
+    const {id} = currentUser
 
-    const mappingProducts = products.map(prod => <ProductCard key={prod.id} prod={prod}/>)
+    const mappingProducts = products.map(prod => <ProductCard key={prod.id} prod={prod} userId={id}/>)
     return (
         <div className="poductsPage">
             <h1>Products Page</h1>
