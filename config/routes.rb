1097d3aec039f0703ee57543f0c29aae3d64b192
Carrie Kraft <carriekarft@gmail.com>
2024-01-28 21:13:42 -0800

    added routes for updating and deleting products

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
     resources :bids, only: [:create, :update]
     patch '/accept_bid/:id', to: "bids#accept_bid"
     resources :comments, only: [:create, :update, :destroy]
-    resources :products, only: [:show, :index, :create, :destroy]
+    resources :products, only: [:show, :index, :create, :update, :destroy]
     resources :users, only: [:index]
+    delete '/user_product_delete/:id', to: "products#user_product_delete"
     post '/signup', to: "users#create"
     get '/me', to: "users#show"
     delete '/logout', to: "sessions#destroy"
